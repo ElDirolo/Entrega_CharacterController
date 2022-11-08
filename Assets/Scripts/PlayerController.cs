a48fd4c93d95e6106e5a5edbfacbf31cfdd61ee0
ElDirolo <rosaldidac@gmail.com>
2022-11-08 16:26:15 +0100

    Elputocontroller de los huevos estoy hasta la polla

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    public CharacterController controller;
+    public CharacterController TUMADRE;
+    private Vector3 playerVelocity; 
+    
     private Animator anim;
     public Transform cam;
     public Transform LookAtTransform;
@@ -19,31 +21,34 @@ public class CharacterController : MonoBehaviour
     public Transform groundSensor;
     public float sensorRadius = 0.1f;
     public LayerMask ground;
-    private Vector3 playerVelocity;
+   
 
     //variables para rotacion del personaje
     private float turnSmoothVelocity;
     public float turnSmoothTime = 0.1f;
 
-    
+    //variables para el movimiento del raton con virtual camera
     public Cinemachine.AxisState xAxis;
     public Cinemachine.AxisState yAxis;
 
     public GameObject[] cameras;
 
-
     // Start is called before the first frame update
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+        //Asignamos el character controller a su variable
+        TUMADRE = GetComponent<CharacterController>();
 
+        //anim = GetComponentInChildren<Animator>();
+
+        //Con esto podemos esconder el icono del raton para que no moleste
         Cursor.lockState = CursorLockMode.Locked;
     }
-
     // Update is called once per frame
     void Update()
     {
         MovementTP();
+        Jump();
     }
 
 
@@ -58,8 +63,8 @@ public class CharacterController : MonoBehaviour
 
         transform.rotation = Quaternion.Euler(0, xAxis.Value, 0);
         LookAtTransform.eulerAngles = new Vector3(yAxis.Value, xAxis.Value, LookAtTransform.eulerAngles.z);
-        
-        
+
+
         Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
         if (Input.GetButton("Fire2"))
         {
@@ -73,7 +78,7 @@ public class CharacterController : MonoBehaviour
         }
 
 
-        
+
         if (move != Vector3.zero)
         {
             float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
@@ -81,28 +86,29 @@ public class CharacterController : MonoBehaviour
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             //controller.Move(moveDirection.normalized * speed * Time.deltaTime);
         }
+        
+    }
 
-        void Jump()
+    void Jump()
+    {
+
+        isGrounded = Physics.CheckSphere(groundSensor.position, sensorRadius, ground);
+
+        //anim.SetBool("Jump", !isGrounded);
+
+        if (isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0;
+        }
+
+        if (isGrounded && Input.GetButtonDown("Jump"))
         {
 
-            isGrounded = Physics.CheckSphere(groundSensor.position, sensorRadius, ground);
-
-            //anim.SetBool("Jump", !isGrounded);
-
-            if (isGrounded && playerVelocity.y < 0)
-            {
-                playerVelocity.y = 0;
-            }
-
-            if (isGrounded && Input.GetButtonDown("Jump"))
-            {
-
-                playerVelocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
-            }
-
-            playerVelocity.y += gravity * Time.deltaTime;
-
-            //controller.Move(playerVelocity * Time.deltaTime);
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
         }
+
+        playerVelocity.y += gravity * Time.deltaTime;
+
+        //controller.Move(playerVelocity * Time.deltaTime);
     }
 }
--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+public class ThirdPersonController : MonoBehaviour
 {
     public CharacterController controller;
     private Animator anim;
@@ -25,41 +25,44 @@ public class CharacterController : MonoBehaviour
     private float turnSmoothVelocity;
     public float turnSmoothTime = 0.1f;
 
-    
+    //variables para el movimiento del raton con virtual camera
     public Cinemachine.AxisState xAxis;
     public Cinemachine.AxisState yAxis;
 
     public GameObject[] cameras;
 
-
     // Start is called before the first frame update
     void Start()
     {
+        //Asignamos el character controller a su variable
         controller = GetComponent<CharacterController>();
 
+        anim = GetComponentInChildren<Animator>();
+
+        //Con esto podemos esconder el icono del raton para que no moleste
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        MovementTP();
+        MovementTPS2();
+        Jump();
     }
 
-
-    void MovementTP()
+    void MovementTPS2()
     {
         float z = Input.GetAxisRaw("Vertical");
-        //anim.SetFloat("VelZ", z);
+        anim.SetFloat("VelZ", z);
         float x = Input.GetAxisRaw("Horizontal");
-        //anim.SetFloat("VelX", x);
+        anim.SetFloat("VelX", x);
+
         xAxis.Update(Time.deltaTime);
         yAxis.Update(Time.deltaTime);
 
         transform.rotation = Quaternion.Euler(0, xAxis.Value, 0);
         LookAtTransform.eulerAngles = new Vector3(yAxis.Value, xAxis.Value, LookAtTransform.eulerAngles.z);
-        
-        
+
+
         Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
         if (Input.GetButton("Fire2"))
         {
@@ -73,7 +76,6 @@ public class CharacterController : MonoBehaviour
         }
 
 
-        
         if (move != Vector3.zero)
         {
             float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
@@ -81,28 +83,38 @@ public class CharacterController : MonoBehaviour
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             //controller.Move(moveDirection.normalized * speed * Time.deltaTime);
         }
+    }
 
-        void Jump()
+    //Funcion de salto y gravedad
+    void Jump()
+    {
+        //Le asignamos a la boleana isGrounded su valor dependiendo del CheckSpher
+        //CheckSphere crea una esfera pasandole la poscion, radio y layer con la que queremos que interactue
+        //si la esfera entra en contacto con la capa que le digamos convertira nuestra boleana en true y si no entra en contacto en false
+        isGrounded = Physics.CheckSphere(groundSensor.position, sensorRadius, ground);
+
+        anim.SetBool("Jump", !isGrounded);
+
+        //Si estamos en el suelo y playervelocity es menor que 0 hacemos que le vuelva a poner el valor a 0
+        //esto es para evitar que siga aplicando fuerza de gravedad cuando estemos en el suelo y evitar comportamientos extraños
+        if (isGrounded && playerVelocity.y < 0)
         {
-
-            isGrounded = Physics.CheckSphere(groundSensor.position, sensorRadius, ground);
-
-            //anim.SetBool("Jump", !isGrounded);
-
-            if (isGrounded && playerVelocity.y < 0)
-            {
-                playerVelocity.y = 0;
-            }
-
-            if (isGrounded && Input.GetButtonDown("Jump"))
-            {
-
-                playerVelocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
-            }
-
-            playerVelocity.y += gravity * Time.deltaTime;
-
-            //controller.Move(playerVelocity * Time.deltaTime);
+            playerVelocity.y = 0;
         }
+
+        //si estamos en el suelo y pulasamos el imput de salto hacemos que salte el personaje
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            //Formula para hacer que los saltos sean de una altura concreta
+            //la altura depende del valor de jumpHeight 
+            //Si jumpHeigt es 1 saltara 1 metro de alto
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
+        //a playervelocity.y le iremos sumando el valor de la gravedad
+        playerVelocity.y += gravity * Time.deltaTime;
+        //como playervelocity en el eje Y es un valor negativo esto nos empuja al personaje hacia abajo
+        //asi le aplicaremos la gravedad
+        //controller.Move(playerVelocity * Time.deltaTime);
     }
 }
